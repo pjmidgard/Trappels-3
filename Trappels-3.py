17304b233c5b84605cf37df4103b66f4af20c811
Java Software <pj.midgard@yahoo.com>
2022-10-27 17:30:31 +0100

    Update Trappels-3.py

--- Trappels-3.py
+++ Trappels-3.py
@@ -358,7 +358,7 @@ class encypthion_class:
                                                                         
                                         
                                         
-                                                                        elif Calculus[Times:Times+4]==Calculus[Times2:Times2+4] and Calculus[Times:Times+4]==Calculus[Times3:Times3+4] and Calculus[Times:Times+4]==Calculus[Times4:Times4+4] and Calculus[Times:Times+4]==Calculus[Times5:Times5+4]   and Times!=Times3 and Times!=Times2 and Times!=Times3 and Times!=Times4 and Times!=Times5 and Times2!=Times and Times2!=Times3 and Times2!=Times4 and Times2!=Times5 and Times3!=Times and Times3!=Times2  and Times3!=Times4 and Times3!=Times5 and Times4!=Times and Times4!=Times2 and Times4!=Times3 and Times3!=Times4 and Times4!=Times5  res3==0 and long2==blocks:
+                                                                        elif Calculus[Times:Times+4]==Calculus[Times2:Times2+4] and Calculus[Times:Times+4]==Calculus[Times3:Times3+4] and Calculus[Times:Times+4]==Calculus[Times4:Times4+4] and Calculus[Times:Times+4]==Calculus[Times5:Times5+4]   and Times!=Times3 and Times!=Times2 and Times!=Times3 and Times!=Times4 and Times!=Times5 and Times2!=Times and Times2!=Times3 and Times2!=Times4 and Times2!=Times5 and Times3!=Times and Times3!=Times2  and Times3!=Times4 and Times3!=Times5 and Times4!=Times and Times4!=Times2 and Times4!=Times3 and Times3!=Times4 and Times4!=Times5 and  res3==0 and long2==blocks:
                                                                             res3=1
 
                                                         
