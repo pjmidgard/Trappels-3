eb5e05a1f67b820561eb5de7fb3389ee820821fc
Java Software <pj.midgard@yahoo.com>
2022-10-28 12:39:33 +0100

    Update Trappels-3.py

--- Trappels-3.py
+++ Trappels-3.py
@@ -912,7 +912,7 @@ class encypthion_class:
                                                                             elif Combination2[Times:Times+4]==Combination2[Times2:Times2+4] and Combination2==Combination3 and Combination2[Times:Times+4]==Combination2[Times3:Times3+4] and Caculus==Combination2 and res1==0 and  Times2!=Times and Times!=Times2 and Times!=Times3 and   long2==blocks and Times<=Times2+8 and Times2<=Times3+8 and Times+8>=Times2 and Times2+8>=Times3 and Combination3!=2**46:
 
 
-                                                                                res9="0"+Combination3
+                                                                                res9=Combination2
                                                                                 res5=1
                                                                                 res1=1
 
@@ -937,7 +937,7 @@ class encypthion_class:
                                                                             elif Combination2[Times:Times+4]==Combination2[Times2:Times2+4] and Caculus==Combination6 and res1==0 and  Times2!=Times and long2==blocks and Combination4!=2**46:
 
 
-                                                                                res9="0"+Combination6
+                                                                                res9=Combination2
                                                                                 res5=1
                                                                                 res1=1
                                                                                 
@@ -950,7 +950,7 @@ class encypthion_class:
 
 
                                                                             elif Combination2[Times:Times+4]==Combination2[Times2:Times2+4] and Combination8==Calculus and Combination2[Times:Times+4]==Combination2[Times3:Times3+4] and Combination2==Caculus and Combination2[Times:Times+4]==Combination2[Times4:Times4+4] and Combination2[Times:Times+4]==Combination2[Times5:Times5+4]    and Times!=Times3 and Times!=Times2 and Times!=Times3 and Times!=Times4 and Times!=Times5 and Times2!=Times and Times2!=Times3 and Times2!=Times4 and Times2!=Times5 and Times3!=Times and Times3!=Times2  and Times3!=Times4 and Times3!=Times5 and Times4!=Times and Times4!=Times2 and Times4!=Times3 and Times3!=Times4 and Times4!=Times5 and  res6==0 and long2==blocks and Combination7!=2**46:
-                                                                                res9="0"+Combination8
+                                                                                res9=Combination2
                                                                                 res5=1
                                                                                 res6=1
 
